4badf95d0727da65f20cf995d64421412abe895c
Asilbek Norboev <asilbeknorboev@gmail.com>
2023-03-04 11:14:17 -0600

    button

---
+++ my-app/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import ButtonSection from '../ButtonSection';
+
+function Home() {
+  return (
+    <div>
+      <ButtonSection></ButtonSection>
+    </div>
+  );
+}
+
+export default Home;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<<<<<<< Updated upstream
 import reportWebVitals from './reportWebVitals';
 
 import Example from './graph';
@@ -18,3 +19,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+=======
+import Example from './graph';
+import Home from './components/pages/Home';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+      <App />
+      <Example/>
+      <Home/>
+    </React.StrictMode>
+  );
+>>>>>>> Stashed changes
